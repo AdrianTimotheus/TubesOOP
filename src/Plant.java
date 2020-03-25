d59bf3d44e81b2d0fa2acc5441ca356f30aa0578
Elisabeth Levana <52229965+ltelisabeth@users.noreply.github.com>
2020-03-25 18:15:53 +0700

    Add files via upload
    plant

---
+++ src/Plant.java
@@ -0,0 +1,10 @@
+public class Plant implements Move {
+    protected int price;
+    protected int shieldpower;
+
+    public Plant (price,gun) {
+        this.price = price;
+        this.shield = power;
+    }
+    
+}
