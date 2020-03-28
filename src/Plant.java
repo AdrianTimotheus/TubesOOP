7e3555bf9c8f132fd5d9e24361d2b20aeea53a20
Elisabeth Levana <52229965+ltelisabeth@users.noreply.github.com>
2020-03-28 13:04:46 +0700

    Update Plant.java

--- src/Plant.java
+++ src/Plant.java
@@ -1,10 +1,19 @@
-public class Plant implements Move {
+public class Plant implements Shield {
+    protected String type;
     protected int price;
     protected int shieldpower;
 
-    public Plant (price,gun) {
+    public Plant (int price,int power) {
         this.price = price;
-        this.shield = power;
+        this.shieldpower = power;
     }
-    
+    public String getType() {
+        return this.type;
+    }
+    public void buy() {
+        System.out.println(" ");
+    }
+    public void gun() {
+        System.out.println(" ");
+    }
 }
