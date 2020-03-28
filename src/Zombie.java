e696980152809bd4414605cf37f8d4e2e74cf07c
Elisabeth Levana <52229965+ltelisabeth@users.noreply.github.com>
2020-03-28 20:00:47 +0700

    Update Zombie.java

--- src/Zombie.java
+++ src/Zombie.java
@@ -6,14 +6,29 @@ public class Zombie implements Attacker {
     protected int attack; //zombie power
     protected int x;
     protected int y;
+    Random random = new Random();
 
     public Zombie (String type,int speed,int attack) {
         this.type = type;
-        this.speed = speed;
-        this.attack = attack;
+        this.speed = speed; // speed = jumlah steps sekali jalan
+        this.attack = attack; 
+        x = random.nextInt(108);
+        y = random.nextInt(4);
     }
     public String getType() {
         return this.type;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public int getSpeed() {
+        return this.speed;
+    }
+    public int getAttack() {
+        return this.attack;
     }
     public void showup() {
         System.out.println(" ");
