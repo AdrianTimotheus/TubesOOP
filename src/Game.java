da1ecb97048703d5a019d5467e0b352d688efa37
Elisabeth Levana <52229965+ltelisabeth@users.noreply.github.com>
2020-03-28 15:31:34 +0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -9,19 +9,29 @@ public class Game {
     } 
     
     public void start() {
-        System.out.print("_____________   ");
-        System.out.print("|            |  | ");
-        System.out.print("|            |  | ");
-        System.out.print("|            |  | ");
-        System.out.print("|            |  | ");
-        System.out.print("|____________|  | ");
-        System.out.print("|               | ");
+        System.out.println("_____________                                                                    __________                                            ");
+        System.out.println("|            |  |                                                                         /                         |                  ");
+        System.out.println("|            |  |                                                                        /                          |                  ");
+        System.out.println("|            |  |                                                                       /                           |                  ");
+        System.out.println("|            |  |                            |                                         /                            |         |        ");
+        System.out.println("|____________|  |   _________   _______  ____|___                    ________         /      _________  __________  |_______    _______");
+        System.out.println("|               |  /         | |       |     |             \\      / |                /       |        | |    |    | |       | | |      |");
+        System.out.println("|               | |          | |       |     |              \\    /  |_______        /        |        | |    |    | |       | | |______|");
+        System.out.println("|               | |          | |       |     |               \\  /           |      /         |        | |    |    | |       | | |       ");
+        System.out.println("|               |  \\_________| |       |     |____            \\/    ________|     /_________ |________| |    |    | |_______| | |_______");
         for (int i=0; i<=3; i++) {
             for (int j=0; j<=107; j++) {
                 screen[i][j] =" ";
             }
         }
     }
+
+    public void menu() {
+        System.out.println("Command pilihan:");
+        System.out.println("1. BUY (Beli Plants)");
+        System.out.println("2. SKIP (Play game)");
+    }
+
     public void print() {
         int index=0;
         for (int i=1; i<=9; i++) {
