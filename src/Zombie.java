f4ae6ed0670fcb91ba77679f721b11ec20be0ff0
Elisabeth Levana <52229965+ltelisabeth@users.noreply.github.com>
2020-03-28 13:04:19 +0700

    Update Zombie.java

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,4 +1,24 @@
-public class Zombie {
-    protected float movementSpeed;
-    
+import java.util.Random;
+
+public class Zombie implements Attacker {
+    protected String type;
+    protected int speed; //zombie speed
+    protected int attack; //zombie power
+    protected int x;
+    protected int y;
+
+    public Zombie (String type,int speed,int attack) {
+        this.type = type;
+        this.speed = speed;
+        this.attack = attack;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public void showup() {
+        System.out.println(" ");
+    }
+    public void attack() {
+        System.out.println(" ");
+    }
 }
